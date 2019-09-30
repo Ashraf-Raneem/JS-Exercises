1344524d1b6fb6418d2fc0d6c1b88433d73b8c2d
cis101 <cis101@secs.iub.edu.bd>
2019-09-29 20:32:15 -0700

    task 2

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 /* JavaScript code here */
-console.log("External JS"); 
+//Task1
+console.log("External JS");
+
+//Task 2 
+
+// Method 1
+/*
+function arraySum (x) {
+    for (var i ; i<x.length; i++){
+        var sum = 0;
+        sum = sum + x[i];
+    }
+    return sum ; 
+}
+var arr = [2,3,4,5,6,7]; 
+var Sum = arraySum (arr); 
+console.log (Sum);
+*/
+
+//Method 2
+function arraySum (arr) {
+    var sum = 0; 
+    /*
+    arr.forEach(function (element){
+        sum= sum + element ; 
+    })
+    return sum ;
+    */
+   return (multiplier) => {
+       return sum*multiplier; 
+   }
+}
+var sum = arraySum([1,2,3]); 
+console.log(sum); 
